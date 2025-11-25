82d4bceefddf836add9282e9b2cb258911a6c0ba
weatry <weatry@gmail.com>
2025-11-25 19:56:31 +0800

    Change birthDate in Searchable

--- src/main/java/com/github/budwing/clean/naming/Searchable.java
+++ src/main/java/com/github/budwing/clean/naming/Searchable.java
@@ -1,4 +1,5 @@
 package com.github.budwing.clean.naming;
+
 public class Searchable {
     public static final String SLOGAN = "USE SEARCHABLE NAMES";
 
@@ -6,14 +7,14 @@ public class Searchable {
      * It's easier to search code with descriptive names.
      */
     private int age = 30;
-    private String birthDate = "1990-01-01";
+    private String birthday = "1990-01-01";
     private String customerId = "C123456";
 
     public void week(String s) {
-        System.out.println("Age: " + age + ", Birth Date: " + birthDate + ", Customer ID: " + customerId);
+        System.out.println("Age: " + age + ", Birth Date: " + birthday + ", Customer ID: " + customerId);
         // DAYS_IN_WEEK is easier to search than 7
         final int DAYS_IN_WEEK = 7;
-        for (int i = 0; i < DAYS_IN_WEEK; i++) { 
+        for (int i = 0; i < DAYS_IN_WEEK; i++) {
             System.out.println("day in a week: " + i);
         }
     }
